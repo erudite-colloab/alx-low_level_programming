c6319524ecc93777561c185d15665de129be2297
erudite-colloab <eugenebaahasamoah@gmail.com>
2023-03-21 14:22:37 +0000

    New

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _absint - function that computes the absolute value of an integer.
+ * @r: input character
+ *
+ * Return: Absolut value of number r
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (-r);
+	}
+	else
+	{
+		return (r);
+	}
+}
